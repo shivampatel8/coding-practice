df6481ed7bc3ce68d74141492748b6a2f0589e4d
shivampatel18 <patel.shivam2@husky.neu.edu>
2019-11-05 23:36:53 -0500

    hammingWeight

---
+++ Leetcode/hamming_weight.py
@@ -0,0 +1,21 @@
+#Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight)
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        x = 1
+        count = 0
+        while n!=0:
+          a = n ^ x
+          x = x<<1
+          if a>=n:
+            continue
+          if n!=a:
+            count+=1
+          n = a
+        return count
+
+if __name__ == '__main__':
+  print(Solution().hammingWeight(15))
