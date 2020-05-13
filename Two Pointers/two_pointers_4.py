5179097f46c60a2a9229ed2c4c157fbb0cc6a92e
shivampatel18 <patel.shivam2@husky.neu.edu>
2020-05-12 20:20:15 -0400

    another one

---
+++ Two Pointers/two_pointers_4.py
@@ -0,0 +1,35 @@
+def search_triplets(arr):
+  triplets = []
+
+  arr.sort()
+  print(arr)
+  for i in range(len(arr)):
+    if i > 0 and arr[i] == arr[i-1]:
+      continue
+    search_pairs(arr,-arr[i],i+1,triplets)
+    # print(triplets)
+  return triplets
+
+def search_pairs(arr, target_sum, left, triplets):
+  right = len(arr) -1
+  while left<right:
+    current_sum = arr[left] + arr[right]
+    if current_sum==target_sum:
+      triplets.append((-target_sum,arr[left],arr[right]))
+      left+=1
+      right-=1
+      while left < right and arr[left] == arr[left-1]:
+        left+=1
+      while left < right and arr[right] == arr[right+1]:
+        right-=1
+    elif current_sum < target_sum:
+      left+=1
+    else:
+      right-=1
+
+def main():
+  print(search_triplets([-3, 0, 1, 2, -1, 1, -2]))
+  print(search_triplets([-5, 2, -1, -2, 3]))
+
+if __name__ == '__main__':
+  main()
